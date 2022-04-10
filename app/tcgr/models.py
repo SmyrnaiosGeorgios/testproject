ea3007207160bde5248a8a7c252544613fd6f5e1
Smyrnaios Georgios <g.smyrnaios@acg.edu>
2022-04-10 12:58:37 +0300

    simple migration test

--- app/tcgr/models.py
+++ app/tcgr/models.py
@@ -5,7 +5,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
 
 class CategoryStore(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=40)
 
     def __str__(self):
         return f"{self.name}"
